011b54499963c3597b60abe3b55db53a293ebdea
xiacijie <cijie@ualberta.ca>
2019-07-20 23:28:47 -0600

    update db to mysql

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ import datetime
 import json
 from flask import jsonify
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///records.sqlite3'
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b51fa35aa80e09:8c155115@us-cdbr-iron-east-02.cleardb.net/heroku_8862be285234577'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 
@@ -15,20 +22,28 @@ class Record(db.Model):
     id = db.Column("record_id",db.Integer,primary_key=True)
     user_name = db.Column(db.String(50))
     commit_message = db.Column(db.String(300))
+    file_name = db.Column(db.String(20))
     commit_time = db.Column(db.DateTime())
     
 
-    def __init__(self,user_name, commit_message, commit_time):
+    def __init__(self,user_name,commit_message, file_name, commit_time):
         self.user_name = user_name
+        self.file_name = file_name
         self.commit_message = commit_message
         self.commit_time = commit_time
+        
 
 @app.route("/")
 def index():
    
     results = []
     for record in Record.query.all():
-        results.append({"id":record.id,"user_name":record.user_name,"commit_message":record.commit_message,"commit_time":record.commit_time.strftime("%Y-%m-%d %H:%M:%S")})
+        id = record.id 
+        user_name = record.user_name
+        commit_message = record.commit_message
+        file_name = record.file_name
+        commit_time = record.commit_time.strftime("%Y-%m-%d %H:%M:%S")
+        results.append({"id":id,"user_name":user_name,"commit_message":commit_message, "file_name":file_name, "commit_time":commit_time})
     return json.dumps(results)
 
 
@@ -36,8 +51,13 @@ def index():
 def push():
     data = json.loads(request.data)
     commits = data["commits"]
+
     for commit in commits:
-        r = Record(commit["author"]["name"],commit["message"],datetime.datetime.now())
+        message = commit["message"]
+        added = commit["added"]
+        if added:
+            file_name = added[0]
+            r = Record(commit["author"]["name"],message,file_name, datetime.datetime.now())
         db.session.add(r)
     db.session.commit()
     return "OK"
